8a49e9bef5b4744361f66d8f7444b21bbe758f3e
uriegas <1930526@upv.edu.mx>
2022-08-15 09:19:11 -0500

    Add initial setup

--- app/src/main/java/com/example/mariela/brazo_robotico/RenderObj.java
+++ app/src/main/java/com/example/mariela/brazo_robotico/RenderObj.java
@@ -12,16 +12,26 @@ import org.rajawali3d.materials.textures.ATexture;
 import org.rajawali3d.math.vector.Vector3;
 import org.rajawali3d.renderer.RajawaliRenderer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class RenderObj extends RajawaliRenderer {
     public Context context;
     private Object3D obj;
-    boolean mostrarOBJ;
+    private boolean mostrarOBJ;
+    private Object3D base, base_arm,                                            // Base
+                     arm_low, arm_high, arm_wrist,                              // Arm
+                     wrist, gear_1, gear_2, link_1, link_2, finger_1, finger_2; // Palm
+    private HashMap<Integer, Object3D> objects; // A map from the file name of an obj and the obj
 
     public RenderObj(Context context, boolean mostrarOBJ) {
         super(context);
         this.context = context;
         this.mostrarOBJ = mostrarOBJ;
         setFrameRate(60);
+        objects = new HashMap<>();
+        objects.put(R.raw.base_arm_obj, null);
+        objects.put(R.raw.waist_arm_obj, null);
     }
 
     public void setmostrarOBJ(boolean mostrarOBJ) {
@@ -34,19 +44,28 @@ public class RenderObj extends RajawaliRenderer {
         key.setPower(2);
         getCurrentScene().addLight(key);
 
-
-        LoaderOBJ loader = new LoaderOBJ(getContext().getResources(), getTextureManager(), R.raw.brazo);
-
         try {
-            loader.parse();
-            obj = loader.getParsedObject();
-            obj.setScale(0.7f);
-//            obj.setScale(5.5f);
-            getCurrentScene().addChild(obj);
+            // Load every object file
+            // TODO: Move LoaderOBJ outside for loop
+            for(Map.Entry<Integer, Object3D> object : objects.entrySet()) {
+                LoaderOBJ loader = new LoaderOBJ(getContext().getResources(), getTextureManager(),
+                                                 object.getKey());
+                loader.parse();
+                objects.put(object.getKey(), loader.getParsedObject());
+                object.getValue().setScale(0.7f); // At this point the object should be loaded
+                getCurrentScene().addChild(object.getValue());
+            }
+
+            // TODO: Initial setup for each object
+            objects.get(R.raw.base_arm_obj).setScale(0.7);
+            objects.get(R.raw.waist_arm_obj).setScale(0.7);
+            objects.get(R.raw.waist_arm_obj).moveUp(0.45f);
+
+            // Set skybox
             getCurrentScene().setSkybox(R.drawable.posx, R.drawable.negx, R.drawable.posy,
                                         R.drawable.negy, R.drawable.posz, R.drawable.negz);
 
-            //obj.moveRight(10.0);
+//            obj.moveRight(10.0);
             //getCurrentScene().addChild(obj);
             //getCurrentCamera().setZ(3.2f);
             //getCurrentCamera().setY(10.5f);
@@ -58,6 +77,7 @@ public class RenderObj extends RajawaliRenderer {
             Log.e("Render", "Skybox not found");
         }
 
+        // Set camera position
         //getCurrentCamera().setPosition(6,20,6);
         getCurrentCamera().setPosition(6,5,5);
         //getCurrentCamera().setLookAt(obj.getPosition());
@@ -78,7 +98,7 @@ public class RenderObj extends RajawaliRenderer {
     public void onRender(final long elapsedTime, final double deltaTime) {
         if (mostrarOBJ == true) {
             super.onRender(elapsedTime, deltaTime);
-//            obj.rotate(Vector3.Axis.Y, 1.0);
+            objects.get(R.raw.waist_arm_obj).rotate(Vector3.Axis.Y, 1.0);
         }
     }
 }
