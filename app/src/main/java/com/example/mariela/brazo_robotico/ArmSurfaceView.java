178a1e75a847f63dbe5bcd39efbc2be6c1b6874f
uriegas <1930526@upv.edu.mx>
2022-08-15 20:41:31 -0500

    Add on drag over skybox laggy

--- app/src/main/java/com/example/mariela/brazo_robotico/ArmSurfaceView.java
+++ app/src/main/java/com/example/mariela/brazo_robotico/ArmSurfaceView.java
@@ -1,2 +1,35 @@
-package com.example.mariela.brazo_robotico;public class ArmSurfaceView {
+package com.example.mariela.brazo_robotico;
+
+import android.content.Context;
+import android.view.MotionEvent;
+
+import org.rajawali3d.surface.IRajawaliSurface;
+import org.rajawali3d.surface.RajawaliSurfaceView;
+
+public class ArmSurfaceView extends RajawaliSurfaceView {
+    private RenderObj render;
+
+    public ArmSurfaceView(Context context, RenderObj render) {
+        super(context);
+        this.setSurfaceRenderer(render);
+        this.render = render;
+        this.setFrameRate(60.0);
+        this.setRenderMode(IRajawaliSurface.RENDERMODE_WHEN_DIRTY);
+    }
+
+    /**
+     * Move camera look_at when user drags over the screen
+     * @param event the given event
+     */
+    @Override
+    public boolean onTouchEvent(final MotionEvent event) {
+        float x = event.getX(), y = event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_MOVE: // Move camera
+                render.setCameraLookAt(x, y, 0);
+//                System.out.println(render.cameraX + ", " + render.cameraY + ", " + render.cameraZ);
+                break;
+        }
+        return true;
+    }
 }
--- app/src/main/java/com/example/mariela/brazo_robotico/MainActivity.java
+++ app/src/main/java/com/example/mariela/brazo_robotico/MainActivity.java
@@ -8,29 +8,36 @@ import org.rajawali3d.surface.RajawaliSurfaceView;
 
 public class MainActivity extends AppCompatActivity {
     RenderObj render;
+    RajawaliSurfaceView surfaceView;
+    Button B1, B2, B3, B4, B5, B6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        render = new RenderObj(this, true);
 
         // Get buttons
-        Button B1 = findViewById(R.id.brazoAbajo);
-        Button B2 = findViewById(R.id.brazoArriba);
-        Button B3 = findViewById(R.id.munecaDerecha);
-        Button B4 = findViewById(R.id.munecaIzquierda);
-        Button B5 = findViewById(R.id.dedosAbrir);
-        Button B6 = findViewById(R.id.dedosCerrar);
+        B1 = findViewById(R.id.brazoAbajo);
+        B2 = findViewById(R.id.brazoArriba);
+        B3 = findViewById(R.id.munecaDerecha);
+        B4 = findViewById(R.id.munecaIzquierda);
+        B5 = findViewById(R.id.dedosAbrir);
+        B6 = findViewById(R.id.dedosCerrar);
 
-        // Set surfaceview
-        final RajawaliSurfaceView surface;
-        surface = findViewById(R.id.gl_surface_view);
-        surface.setFrameRate(60.0);
-        surface.setRenderMode(IRajawaliSurface.RENDERMODE_WHEN_DIRTY);
+        // Traditional surfaceView
+        surfaceView = findViewById(R.id.gl_surface_view);
+        surfaceView.setFrameRate(60.0);
+        surfaceView.setSurfaceRenderer(render);
+        surfaceView.setRenderMode(IRajawaliSurface.RENDERMODE_WHEN_DIRTY);
 
-        render = new RenderObj(this, true);
-        surface.setSurfaceRenderer(render);
+        // SurfaceView with onTouch over skybox
+        // TODO: Change this to inflate, so we can get the buttons of the xml
+//        render = new RenderObj(this, true);
+//        surfaceView = new ArmSurfaceView(this, render);
+//        setContentView(surfaceView);
 
+        // Set buttons actions
         B2.setOnClickListener(view -> { // rotate arm clockwise
 //            if (render.angle_1 < 135.0) render.angle_1 += render.ANGLE_STEP;
         });
