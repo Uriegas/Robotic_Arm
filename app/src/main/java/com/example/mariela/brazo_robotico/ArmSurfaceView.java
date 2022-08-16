42965530e0f43525258e7ae847a7ad9c49ec6bfd
uriegas <1930526@upv.edu.mx>
2022-08-15 20:41:06 -0500

    Add textures to objects

---
+++ app/src/main/java/com/example/mariela/brazo_robotico/ArmSurfaceView.java
@@ -0,0 +1,2 @@
+package com.example.mariela.brazo_robotico;public class ArmSurfaceView {
+}
--- app/src/main/java/com/example/mariela/brazo_robotico/RenderObj.java
+++ app/src/main/java/com/example/mariela/brazo_robotico/RenderObj.java
@@ -1,6 +1,7 @@
 package com.example.mariela.brazo_robotico;
 
 import android.content.Context;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.MotionEvent;
 
@@ -8,22 +9,32 @@ import org.rajawali3d.Object3D;
 import org.rajawali3d.lights.DirectionalLight;
 import org.rajawali3d.loader.LoaderOBJ;
 import org.rajawali3d.loader.ParsingException;
+import org.rajawali3d.materials.Material;
+import org.rajawali3d.materials.methods.DiffuseMethod;
+import org.rajawali3d.materials.methods.SpecularMethod;
 import org.rajawali3d.materials.textures.ATexture;
+import org.rajawali3d.materials.textures.Texture;
 import org.rajawali3d.math.vector.Vector3;
 import org.rajawali3d.renderer.RajawaliRenderer;
+import org.w3c.dom.Text;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RenderObj extends RajawaliRenderer {
+    private final String TAG = "Renderer";
     private float scale_factor = 1.5f;
+    public float cameraX = 0, cameraY = 2, cameraZ = 0; // Initial values of the camera look at
+    public float canvasX = 0, canvasY = 0, canvasZ = 0;
+    private float moveSpeed = 0.02f;
     public Context context;
-    private Object3D obj;
     private boolean mostrarOBJ;
+    private HashMap<Integer, Object3D> objects; // A map from the file name of an obj and the obj
+
+    // For reference; will be deleted anyways by the garbage collector
     private Object3D base, base_arm,                                            // Base
                      arm_low, arm_high, arm_wrist,                              // Arm
                      wrist, gear_1, gear_2, link_1, link_2, finger_1, finger_2; // Palm
-    private HashMap<Integer, Object3D> objects; // A map from the file name of an obj and the obj
 
     public RenderObj(Context context, boolean mostrarOBJ) {
         super(context);
@@ -42,11 +53,31 @@ public class RenderObj extends RajawaliRenderer {
 
     @Override
     protected void initScene() {
+        // Set lighting for the scene
         DirectionalLight key = new DirectionalLight(-3,-4,-5);
         key.setPower(2);
         getCurrentScene().addLight(key);
 
+        // Material for blue parts (texture and more)
+        Material mBlue = new Material();
+        mBlue.enableLighting(true);
+        mBlue.setDiffuseMethod(new DiffuseMethod.Lambert()); // Use the lambert method for lighting
+        mBlue.setColorInfluence(0); // TODO experiment this
+        Texture blueTexture = new Texture("Blue", R.drawable.blue_texture);
+
+        // Material for silver parts (texture and more)
+        Material mSilver = new Material();
+        mSilver.enableLighting(true);
+        mSilver.setDiffuseMethod(new DiffuseMethod.Lambert());
+        mSilver.setColorInfluence(0);
+        mSilver.setSpecularMethod(new SpecularMethod.Phong());
+        Texture silverTexture = new Texture("Silver", R.drawable.silver_texture);
+
         try {
+            // Try adding the textures to the materials
+            mBlue.addTexture(blueTexture);
+            mSilver.addTexture(silverTexture);
+
             // Load every object file
             // TODO: Move LoaderOBJ outside for loop
             for(Map.Entry<Integer, Object3D> object : objects.entrySet()) {
@@ -60,18 +91,20 @@ public class RenderObj extends RajawaliRenderer {
             // TODO: Initial setup for each object
             // Base of arm
             objects.get(R.raw.base_arm_obj).setScale(0.7 * scale_factor);
+            objects.get(R.raw.base_arm_obj).setMaterial(mSilver);
             // Base 2 of the arm
             objects.get(R.raw.waist_arm_obj).setScale(0.7 * scale_factor);
             objects.get(R.raw.waist_arm_obj).rotate(Vector3.Axis.Y, 90);
             objects.get(R.raw.waist_arm_obj).moveUp(0.43f * scale_factor);
             objects.get(R.raw.waist_arm_obj).moveRight(-0.010f);
             objects.get(R.raw.waist_arm_obj).moveForward(-0.048f);
+            objects.get(R.raw.waist_arm_obj).setMaterial(mBlue);
             // Arm low
             objects.get(R.raw.arm_01_obj).setScale(1.7 * scale_factor);
             objects.get(R.raw.arm_01_obj).moveUp(0.6 * scale_factor);
             objects.get(R.raw.arm_01_obj).moveRight(0.150f);
             objects.get(R.raw.arm_01_obj).moveForward(0.05f);
-
+            objects.get(R.raw.arm_01_obj).setMaterial(mBlue);
 
             // Set skybox
             getCurrentScene().setSkybox(R.drawable.posx, R.drawable.negx, R.drawable.posy,
@@ -86,7 +119,7 @@ public class RenderObj extends RajawaliRenderer {
         } catch (ParsingException e) {
             e.printStackTrace();
         } catch (ATexture.TextureException e ) {
-            Log.e("Render", "Skybox not found");
+            Log.d(TAG + ".initScene", e.toString());
         }
 
         // Set camera position
@@ -100,7 +133,7 @@ public class RenderObj extends RajawaliRenderer {
 //        getCurrentCamera().setLookAt(objects.get(R.raw.waist_arm_obj).getPosition());
         // Front view
         getCurrentCamera().setPosition(0, 5, 5);
-        getCurrentCamera().setLookAt(0,2,0);
+        getCurrentCamera().setLookAt(cameraX,cameraY,cameraZ);
     }
 
     @Override
@@ -108,9 +141,35 @@ public class RenderObj extends RajawaliRenderer {
 
     }
 
+    /**
+     * The implementation of onTouch is in {@link ArmSurfaceView}
+     */
     @Override
-    public void onTouchEvent(MotionEvent event) {
-        // TODO: Move around the skybox in drag
+    public void onTouchEvent(MotionEvent event) { }
+
+    /**
+     * This is a work-around to the fact that mapping the Android plane to the Rajawali plane is
+     * hard since we can't get the direction of any move operation, thus we have to implement memory
+     * of the previous position
+     * <p>
+     * We don't modify the <bold>z</bold> coordinate since we don't want to modify the zoom of the
+     * camera yet but this could be implemented later.
+     * <p>
+     * HINT: {@link ArmSurfaceView#onTouchEvent(MotionEvent)} should handle this with gestures
+     * @param x x coordinate
+     * @param y y coordinate
+     * @param z z coordinate
+     */
+    public void setCameraLookAt(float x, float y, float z) {
+        // TODO: Movement is not as straight forward, get direction of the finger
+        cameraX += (x - canvasX < 0 ? 1 : -1) * moveSpeed;
+        cameraY += (y - canvasY > 0 ? 1 : -1) * moveSpeed;
+        // cameraZ = += (z - canvasZ > 0 ? 1 : -1) * moveSpeed;
+        // Set new canvas positions
+        canvasX = x; canvasY = y;
+
+        // Move lookAt in camera
+        getCurrentCamera().setLookAt(cameraX, cameraY, cameraZ);
     }
 
     @Override
