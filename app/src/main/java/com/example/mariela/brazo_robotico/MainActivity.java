184b606aed6f18ef903e635c2661ce6de583b92c
uriegas <1930526@upv.edu.mx>
2022-08-18 07:50:15 -0500

    More buttons

--- app/src/main/java/com/example/mariela/brazo_robotico/MainActivity.java
+++ app/src/main/java/com/example/mariela/brazo_robotico/MainActivity.java
@@ -9,7 +9,7 @@ import org.rajawali3d.surface.RajawaliSurfaceView;
 public class MainActivity extends AppCompatActivity {
     RenderObj render;
     RajawaliSurfaceView surfaceView;
-    Button B1, B2, B3, B4, B5, B6;
+    Button B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, B12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +18,18 @@ public class MainActivity extends AppCompatActivity {
         render = new RenderObj(this);
 
         // Get buttons
-        B1 = findViewById(R.id.brazoArriba);
-        B2 = findViewById(R.id.brazoAbajo);
-        B3 = findViewById(R.id.munecaIzquierda);
-        B4 = findViewById(R.id.munecaDerecha);
-        B5 = findViewById(R.id.dedosAbrir);
-        B6 = findViewById(R.id.dedosCerrar);
+        B1 = findViewById(R.id.B1);
+        B2 = findViewById(R.id.B2);
+        B3 = findViewById(R.id.B3);
+        B4 = findViewById(R.id.B4);
+        B5 = findViewById(R.id.B5);
+        B6 = findViewById(R.id.B6);
+//        B7 = findViewById(R.id.B7);
+//        B8 = findViewById(R.id.B8);
+        B9 = findViewById(R.id.B9);
+        B10 = findViewById(R.id.B10);
+        B11 = findViewById(R.id.B11);
+        B12 = findViewById(R.id.B12);
 
         // Traditional surfaceView
         surfaceView = findViewById(R.id.gl_surface_view);
@@ -39,10 +45,16 @@ public class MainActivity extends AppCompatActivity {
 
         // Set buttons actions
         B1.setOnClickListener(view -> { render.rotateBase(true); });
-        B2.setOnClickListener(view -> { render.rotateArmLow(true); });
-        B3.setOnClickListener(view -> { render.rotateArmHigh(true); });
-        B4.setOnClickListener(view -> { render.rotateArmWristAround(true);});
-        B5.setOnClickListener(view -> { render.rotateArmWrist(true);});
-        B6.setOnClickListener(view -> { render.openHand(true);});
+        B2.setOnClickListener(view -> { render.rotateBase(false); });
+        B3.setOnClickListener(view -> { render.rotateArmLow(true); });
+        B4.setOnClickListener(view -> { render.rotateArmLow(false);});
+        B5.setOnClickListener(view -> { render.rotateArmHigh(true);});
+        B6.setOnClickListener(view -> { render.rotateArmHigh(false);});
+//        B7.setOnClickListener(view -> { render.rotateArmWristAround(true); });
+//        B8.setOnClickListener(view -> { render.rotateArmWristAround(false); });
+        B9.setOnClickListener(view -> { render.rotateArmWrist(true); });
+        B10.setOnClickListener(view -> { render.rotateArmWrist(false);});
+        B11.setOnClickListener(view -> { render.openHand(true);});
+        B12.setOnClickListener(view -> { render.openHand(false);});
     }
 }
